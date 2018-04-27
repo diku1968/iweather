0172beb587a056ef6c28a75760bbb957bffaadaf
diku1968 <diku1968@gmail.com>
2018-04-27 15:06:58 +0530

    Your Awesome Changes!

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,16 @@
+import { Http} from '@angular/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
+@Injectable()
+export class WeatherProvider {
+ apikey = 'e265e3942a517b28';
+ url;
+  constructor(public http: Http) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apikey+'/conditions/q'
+  }
+  getWeather(city,state){
+    return this.http.get(this.url+'/'+state+'/' + city + '.json')
+    .map(res => res.json());
+  }
+}
